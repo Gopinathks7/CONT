1087eec0a6aa4f1b62c4b8b008ad2f3e7e216a10
Gopinath kumsi <gopinathks9@gmail.com>
2020-06-04 13:03:17 +0530

    gopi

--- src/test/java/CONT/CONT/App.java
+++ src/test/java/CONT/CONT/App.java
@@ -1,8 +1,11 @@
 package CONT.CONT;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -24,7 +27,16 @@ public class App extends MyReport
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-        driver.get("https://www.google.com/");
+        driver.get("E:\\Demohtml\\Tables.html");
+        String xpath1=".//tr[2]/td[2]";
+        List<WebElement> one = driver.findElements(By.xpath(xpath1));
+        int count=one.size();
+        for(int i=0;i<count;i++)
+        {
+        	WebElement el= one.get(i);
+        	String name=el.getText();
+        	System.out.println(name);
+        }
     }
 	   catch(Exception e)
 	   {
